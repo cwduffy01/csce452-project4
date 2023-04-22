6745bf02bee5f42964274f76facd407e19bdd2c2
Carson Duffy <cwduffy01@gmail.com>
2023-04-21 19:29:57 -0500

    simulation commenting

--- src/project4/project4/simulation.py
+++ src/project4/project4/simulation.py
@@ -44,14 +44,16 @@ def quaternion_from_euler(ai, aj, ak):
     return q
 
 
+# convert LaserScan to PointCloud
 def laser_scan_to_point_cloud(scan):
     return PointCloud(header=scan.header, points = [ Point32(x=scan.ranges[i]*np.cos(scan.angle_min+i*scan.angle_increment), y=scan.ranges[i]*np.sin(scan.angle_min+i*scan.angle_increment), z=0.0) for i in range(len(scan.ranges)) ])
 
 
+# Node that runs the simulation
 class Simulation(Node):
 
     move_timer = 0.1
-    no_move_instruction = 0
+    no_move_instruction = 0     # counter for the amount of move instructions
     vl_err = 1
     vr_err = 1
     vl = 0
@@ -61,6 +63,7 @@ class Simulation(Node):
 
         super().__init__('Simulation')
 
+        # pull parameters from command line 
         self.declare_parameters(
             namespace='',
             parameters=[
@@ -74,36 +77,34 @@ class Simulation(Node):
         self.world, world_string = get_occupancy_grid(self.get_parameter('world_file').value)
         self.obstacle_lines = vectorize(world_string, self.world['resolution'])
 
-        # inputs
+        # input topics
         self.vl_sub = self.create_subscription(Float64, '/vl', self.set_vl, 10)
         self.vr_sub = self.create_subscription(Float64, '/vr', self.set_vr, 10)
 
-        # outputs
-        self.occupancy_pub = self.create_publisher(OccupancyGrid, '/map', 10)
-        self.laser_pub = self.create_publisher(LaserScan, '/scan', 10)
-        # self.laser_sub = self.create_subscription(LaserScan, '/scan', self.publish_points, 10)
-        self.pc_pub = self.create_publisher(PointCloud, '/pc', 10)
+        # output topics 
+        self.occupancy_pub = self.create_publisher(OccupancyGrid, '/map', 10)   # topic for map
+        self.laser_pub = self.create_publisher(LaserScan, '/scan', 10)          # topic for laser scan
+        self.pc_pub = self.create_publisher(PointCloud, '/pc', 10)              # topic for point cloud
 
-        self.timer = self.create_timer(self.robot["laser"]["rate"], self.generate_lidar)
+        # timed actions
+        self.robot_timer = self.create_timer(self.move_timer, self.move_robot)  # move robot
+        self.error_timer = self.create_timer(self.robot['wheels']['error_update_rate'], self.update_errors)     # update wheel errors
+        self.lidar_timer = self.create_timer(self.robot["laser"]["rate"], self.generate_lidar)  # lidar point generated 
 
         # set intial pose
         self.x = self.world['initial_pose'][0]
         self.y = self.world['initial_pose'][1]
         self.theta = self.world['initial_pose'][2]
 
-        # transforms
-        self.world_base_broadcast = TransformBroadcaster(self)
-
-        # timers
-        self.robot_timer = self.create_timer(self.move_timer, self.move_robot) # move robot
-        self.error_timer = self.create_timer(self.robot['wheels']['error_update_rate'], self.update_errors) # update wheel errors
-
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
-
         # set initial error
         self.vr_err = np.random.normal(1, math.sqrt(self.robot['wheels']['error_variance_left']))
         self.vl_err = np.random.normal(1, math.sqrt(self.robot['wheels']['error_variance_right']))
+
+        # transform variables
+        self.world_base_broadcast = TransformBroadcaster(self)
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
 
         # publish occupancy grid
         header = Header()
@@ -121,18 +122,23 @@ class Simulation(Node):
 
         self.occupancy_pub.publish(og)
 
+
+    # left velocity listener
     def set_vl(self, msg):
         self.vl = msg.data
         self.no_move_instruction = 0
 
+    # right velocity listener
     def set_vr(self, msg):
         self.vr = msg.data
         self.no_move_instruction = 0
 
+    # error update listener
     def update_errors(self):
         self.vr_err = np.random.normal(1, math.sqrt(self.robot['wheels']['error_variance_left']))
         self.vl_err = np.random.normal(1, math.sqrt(self.robot['wheels']['error_variance_right']))
 
+    # robot motion listener
     def move_robot(self):
         # if no instruction for 1 second, dont move
         if(self.no_move_instruction >= 1/self.move_timer):
@@ -145,17 +151,18 @@ class Simulation(Node):
         # compute new state
         l = self.robot['wheels']['distance'] # distance between wheels
 
-        if(vl == vr): # go straight
+        # handle three potential motions
+        if(vl == vr):   # go straight
             x = self.x + math.cos(self.theta) * vl * self.move_timer
             y = self.y + math.sin(self.theta) * vl * self.move_timer
             new_state = [x, y, self.theta]
 
-        elif(vl == vr * -1): # turn in place
+        elif(vl == vr * -1):    # turn in place
             w = (vr - vl) / l
             theta = self.theta + w * self.move_timer
             new_state = [self.x, self.y, theta]
 
-        else:
+        else:   # rotation 
             R = l/2 * (vr + vl) / (vr - vl) # distance to ICC
             w = (vr - vl) / l # angular velocity around ICC
             c = [self.x - R * math.sin(self.theta), self.y + R * math.cos(self.theta)] # location of ICC
@@ -171,99 +178,106 @@ class Simulation(Node):
             new_state = m1 * v1 + v2
             new_state = new_state.flatten().tolist()[0]
 
-        # do collision detection
+        # collision detection
         for line in self.obstacle_lines:
             dist = point_line_distance(line[0], line[1], line[2], line[3], new_state[0], new_state[1])
             if dist < self.robot['body']['radius']:
                 return
 
+        # set the state to the new state
         self.x = new_state[0]
         self.y = new_state[1]
         self.theta = new_state[2]
 
+        # publish transform message
         t = TransformStamped()
-
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = 'world'
         t.child_frame_id = 'base_link'
 
+        # set translation of transform
         t.transform.translation.x = self.x
         t.transform.translation.y = self.y
         t.transform.translation.z = 0.0
 
+        # generate quaternion from the euler angles of the robot
         quat = quaternion_from_euler(0, 0, self.theta)
 
+        # set rotation of transform to quaternion values
         t.transform.rotation.x = quat[0]
         t.transform.rotation.y = quat[1]
         t.transform.rotation.z = quat[2]
         t.transform.rotation.w = quat[3]
 
+        # send transform
         self.world_base_broadcast.sendTransform(t)
 
+        # count movement instruction
         self.no_move_instruction += 1
 
+    # generate lidar points listener
     def generate_lidar(self): 
-        ls = LaserScan()
-
         robot_laser = self.robot["laser"]
-
-        ls.header.frame_id = "laser" 
-
         num_scans = robot_laser["count"]
 
+        # create laser scan message
+        ls = LaserScan()
+        ls.header.frame_id = "laser" 
+        ls.header.stamp = trans_time.to_msg()
         ls.angle_min = robot_laser["angle_min"]
         ls.angle_max = robot_laser["angle_max"]
         ls.angle_increment = (ls.angle_max - ls.angle_min) / num_scans
         ls.scan_time = float(robot_laser["rate"]) 
-
         ls.range_min = robot_laser["range_min"]
         ls.range_max = robot_laser["range_max"]
-
-        # CHANGE TO PUT POINTS ON MAP
         ls.ranges = []
         ls.intensities = np.zeros(num_scans, dtype=float).tolist()
 
+        # get laser transform and times
         trans_time = rclpy.time.Time()
-
-        # transform laser position to world frame
         start_time = time.time()
         laser_to_world_tf = self.tf_buffer.lookup_transform(
             "world",
             "laser",
             trans_time)
 
+        # get translation and rotation of laser frame
         laser_x = laser_to_world_tf.transform.translation.x
         laser_y = laser_to_world_tf.transform.translation.y
         q = laser_to_world_tf.transform.rotation
         laser_theta = np.arctan2(2 * (q.w * q.z + q.x * q.y), 1 - 2 * (q.y ** 2 + q.z ** 2))
 
+        # for each angle, get point
         curr_angle = laser_theta + ls.angle_min
         for i in range(num_scans): 
+            # account for failing scan
             rand = np.random.rand()
             if rand < self.robot['laser']['fail_probability']:
                 ls.ranges.append(float('nan'))
                 curr_angle += ls.angle_increment
                 continue
 
-            min_dist = float("inf")   # maybe change
-            for seg in self.obstacle_lines:
-                intersect = line_ray_intersection(laser_x, laser_y, curr_angle, *seg)
-                # self.get_logger().info("intersect distance: %d" % (intersect))
-                if intersect > ls.range_min**2 and intersect < ls.range_max**2:
-                    if intersect < min_dist**2 or min_dist == float("inf"):
+            # get minimum distance for each scan 
+            min_dist = float("inf")
+            for seg in self.obstacle_lines:     # get point for each segment
+                intersect = line_ray_intersection(laser_x, laser_y, curr_angle, *seg)   # get distance to line segment
+                if intersect > ls.range_min**2 and intersect < ls.range_max**2:         # check within range
+                    if intersect < min_dist**2 or min_dist == float("inf"):             # check if it is lower than 
                         min_dist = intersect
+
+            # add scan error
             err = np.random.normal(0, math.sqrt(self.robot['laser']['error_variance']))
             ls.ranges.append(math.sqrt(min_dist) + err)
+
+            # next angle
             curr_angle += ls.angle_increment
 
-        
+        # set time increment of scan
         end_time = time.time()
         time_change = (end_time - start_time) / 1000 + float(robot_laser["rate"])
-        self.get_logger().info(f"{start_time}, {end_time}, {time_change}")
         ls.time_increment = time_change
-        
-        ls.header.stamp = trans_time.to_msg()
 
+        # publish point cloud points
         pc = laser_scan_to_point_cloud(ls)
         self.pc_pub.publish(pc)
         self.laser_pub.publish(ls)
